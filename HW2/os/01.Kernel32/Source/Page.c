ab4e36e27d3cdbf1ba8e495573ff0d8f364aec9a
taekwan-hwang <ethan.taekwanhwang@gmail.com>
2019-10-09 15:39:33 +0900

    Add Comment

--- HW2/os/01.Kernel32/Source/Page.c
+++ HW2/os/01.Kernel32/Source/Page.c
@@ -8,18 +8,21 @@ void kInitializePageTables(void){
 	DWORD dwMappingAddress;
 	int i;
 
-	// Create Table PML4
+	// PML4 테이블 생성
+	// 첫 번째 엔트리 외에 나머지는 모두 0으로 초기화
 	pstPML4TEntry = ( PML4TENTRY* ) 0x100000;
 	kSetPageEntryData( &( pstPML4TEntry[ 0 ] ), 0x00, 0x101000, PAGE_FLAGES_DEFAULT, 0);
 	for( i=1; i<PAGE_MAXENTRYCOUNT; i++ ){
 		kSetPageEntryData( &( pstPML4TEntry[ i ] ), 0, 0, 0, 0 );
 	}
 
-	// Create Table Directory Pointer
+	// 페이지 디렉터리 포인터 테이블 생성
+	// 하나의 PDPT로 512GByte까지 매핑 가능하므로 하나로 충분함
+	// 64개의 엔트리를 설정하여 64GByte까지 매핑함
 	pstPDPTEntry = ( PDPTENTRY* ) 0x101000;
 	for( i =0; i<64; i++ ){
 		kSetPageEntryData( &( pstPDPTEntry[ i ] ), 0, 0x102000 + ( i*PAGE_TABLESIZE ), PAGE_FLAGES_DEFAULT, 0 );
-			}
+	}
 	for( i=64; i < PAGE_MAXENTRYCOUNT; i++ ){
 		kSetPageEntryData( &( pstPDPTEntry[ i ] ), 0, 0, 0, 0 );
 	}
@@ -32,6 +35,12 @@ void kInitializePageTables(void){
 	
 		dwMappingAddress += PAGE_DEFAULTSIZE;
 	}
+
+	// Create Table Page
+	pstPTEntry = (PTENTRY*) 0x103000;
+	for(i = 0; i < )
+	//for (i = 0; i < PAGE_MAX)
+
 }
 
 // Set Flag to Page Entry
