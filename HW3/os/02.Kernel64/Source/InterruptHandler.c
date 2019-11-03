5e54b4671392dec813cb8e1cb4c4723658460358
hyebeen <hyebeen0328@naver.com>
2019-11-04 02:55:45 +0900

    .

--- HW3/os/02.Kernel64/Source/InterruptHandler.c
+++ HW3/os/02.Kernel64/Source/InterruptHandler.c
@@ -1,27 +1,100 @@
-/**
- *  file    InterruptHandler.c
- *  date    2009/01/24
- *  author  kkamagui
- *          Copyright(c)2008 All rights reserved by kkamagui
- *  brief   인터럽트 및 예외 핸들러에 관련된 소스 파일
- */
-
 #include "InterruptHandler.h"
 #include "PIC.h"
 #include "Keyboard.h"
 #include "Console.h"
 
-/**
- *  공통으로 사용하는 예외 핸들러
- */
+void kSetPageEntryData(PTENTRY *pstEntry, DWORD dwUpperBaseAddress, DWORD dwLowerBaseAddress, DWORD dwLowerFlags, DWORD dwUpperFlags)
+{
+    pstEntry->dwAttributeAndLowerBaseAddress = dwLowerBaseAddress | dwLowerFlags;
+    pstEntry->dwUpperBaseAddressAndEXB = (dwUpperBaseAddress & 0xFF) | dwUpperFlags;
+}
+
+void kDistinguishException(int iVectorNumber, QWORD qwErrorCode)
+{
+    int page_mask = 0x00000001;
+    int protection_mask = 0x00000002;
+
+    if ((qwErrorCode & page_mask) == 0)
+    {
+        kPageFaultExceptionHandler(iVectorNumber, qwErrorCode);
+    }
+    else if ((qwErrorCode & protection_mask) == 2)
+    {
+        kProtectionFaultExceptionHandler(iVectorNumber, qwErrorCode);
+    }
+}
+void kPageFaultExceptionHandler(int iVectorNumber, QWORD qwErrorCode)
+{
+    char vcBuffer[7] = {
+        0,
+    };
+    int number = 0;
+    int mask = 0x00F00000;
+    PTENTRY *pstPTEntry;
+
+    for (int i = 0; i < 6; i++)
+    {
+        if ((number = ((iVectorNumber & mask) >> (20 - i * 4))) <= 9)
+        {
+            vcBuffer[i] = '0' + number;
+        }
+        else
+        {
+            vcBuffer[i] = 87 + number;
+        }
+        mask >>= 4;
+    }
+
+    kPrintf("====================================================\n");
+    kPrintf("                 Page Fault Occur~!!!!               \n");
+    kPrintf("                    Address: 0x%s ", vcBuffer);
+    kPrintf("\n====================================================\n");
+
+    pstPTEntry = (PTENTRY *)0x142000;
+    kSetPageEntryData(&(pstPTEntry[511]), 0, 0x1FF000, 0x00000003, 0); //0
+
+    invlpg(&iVectorNumber);
+    kPrintf("hi3");
+}
+void kProtectionFaultExceptionHandler(int iVectorNumber, QWORD qwErrorCode)
+{
+    char vcBuffer[7] = {
+        0,
+    };
+    int number = 0;
+    int mask = 0x00F00000;
+    PTENTRY *pstPTEntry;
+
+    for (int i = 0; i < 6; i++)
+    {
+        if ((number = ((iVectorNumber & mask) >> (20 - i * 4))) <= 9)
+            vcBuffer[i] = '0' + number;
+        else
+            vcBuffer[i] = 87 + number;
+        mask >>= 4;
+    }
+
+    kPrintStringXY(0, 0, "====================================================");
+    kPrintStringXY(0, 1, "                 Protection Fault Occur~!!!!               ");
+    kPrintStringXY(0, 2, "                    Address:                         ");
+    kPrintStringXY(28, 2, "0x");
+    kPrintStringXY(30, 2, vcBuffer);
+    kPrintStringXY(0, 3, "====================================================");
+
+    pstPTEntry = (PTENTRY *)0x142000;
+    kSetPageEntryData(&(pstPTEntry[511]), 0, 0x1FF000, 0x00000003, 0);
+    invlpg(&iVectorNumber);
+}
+
+
 void kCommonExceptionHandler( int iVectorNumber, QWORD qwErrorCode )
 {
     char vcBuffer[ 3 ] = { 0, };
 
-    // 인터럽트 벡터를 화면 오른쪽 위에 2자리 정수로 출력
+   
     vcBuffer[ 0 ] = '0' + iVectorNumber / 10;
     vcBuffer[ 1 ] = '0' + iVectorNumber % 10;
-    
+
     kPrintStringXY( 0, 0, "====================================================" );
     kPrintStringXY( 0, 1, "                 Exception Occur~!!!!               " );
     kPrintStringXY( 0, 2, "                    Vector:                         " );
@@ -31,56 +104,51 @@ void kCommonExceptionHandler( int iVectorNumber, QWORD qwErrorCode )
     while( 1 ) ;
 }
 
-/**
- *  공통으로 사용하는 인터럽트 핸들러
- */
 void kCommonInterruptHandler( int iVectorNumber )
 {
     char vcBuffer[] = "[INT:  , ]";
     static int g_iCommonInterruptCount = 0;
 
-    //=========================================================================
-    // 인터럽트가 발생했음을 알리려고 메시지를 출력하는 부분
-    // 인터럽트 벡터를 화면 오른쪽 위에 2자리 정수로 출력
     vcBuffer[ 5 ] = '0' + iVectorNumber / 10;
     vcBuffer[ 6 ] = '0' + iVectorNumber % 10;
-    // 발생한 횟수 출력
+   
     vcBuffer[ 8 ] = '0' + g_iCommonInterruptCount;
     g_iCommonInterruptCount = ( g_iCommonInterruptCount + 1 ) % 10;
     kPrintStringXY( 70, 0, vcBuffer );
-    //=========================================================================
-    
-    // EOI 전송
+   
+
+    // EOI 
     kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
 }
 
-/**
- *  키보드 인터럽트의 핸들러
- */
 void kKeyboardHandler( int iVectorNumber )
 {
     char vcBuffer[] = "[INT:  , ]";
     static int g_iKeyboardInterruptCount = 0;
     BYTE bTemp;
 
-    //=========================================================================
-    // 인터럽트가 발생했음을 알리려고 메시지를 출력하는 부분
-    // 인터럽트 벡터를 화면 왼쪽 위에 2자리 정수로 출력
+    
     vcBuffer[ 5 ] = '0' + iVectorNumber / 10;
     vcBuffer[ 6 ] = '0' + iVectorNumber % 10;
-    // 발생한 횟수 출력
+    
     vcBuffer[ 8 ] = '0' + g_iKeyboardInterruptCount;
     g_iKeyboardInterruptCount = ( g_iKeyboardInterruptCount + 1 ) % 10;
     kPrintStringXY( 0, 0, vcBuffer );
-    //=========================================================================
-
-    // 키보드 컨트롤러에서 데이터를 읽어서 ASCII로 변환하여 큐에 삽입
+    //====================================================
     if( kIsOutputBufferFull() == TRUE )
     {
         bTemp = kGetKeyboardScanCode();
         kConvertScanCodeAndPutQueue( bTemp );
     }
 
-    // EOI 전송
+    // EOI 
     kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
+}
+
+static inline void invlpg(void *m)
+{
+    asm volatile("invlpg (%0)"
+                 :
+                 : "b"(m)
+                 : "memory");
 }
