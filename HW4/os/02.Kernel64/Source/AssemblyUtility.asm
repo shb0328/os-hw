12bb56396024f28b9676db5c0138720d5a09b186
hyebeen <hyebeen0328@naver.com>
2019-11-20 02:29:49 +0900

    kReadTSC kSwitchContext kHlt kTestAndSet

--- HW4/os/02.Kernel64/Source/AssemblyUtility.asm
+++ HW4/os/02.Kernel64/Source/AssemblyUtility.asm
@@ -11,6 +11,8 @@ SECTION .text       ; text 섹션(세그먼트)을 정의
 ; C 언어에서 호출할 수 있도록 이름을 노출함(Export)
 global kInPortByte, kOutPortByte, kLoadGDTR, kLoadTR, kLoadIDTR
 global kEnableInterrupt, kDisableInterrupt, kReadRFLAGS
+global kReadTSC
+global kSwitchContext, kHlt, kTestAndSet
 
 ; 포트로부터 1바이트를 읽음
 ;   PARAM: 포트 번호
@@ -81,4 +83,153 @@ kReadRFLAGS:
     pushfq                  ; RFLAGS 레지스터를 스택에 저장
     pop rax                 ; 스택에 저장된 RFLAGS 레지스터를 RAX 레지스터에 저장하여
                             ; 함수의 반환 값으로 설정
+    ret
+
+;타임 스탬프 카운터(TSC)를 읽어서 반환    
+kReadTSC:
+    push rdx                  
+    
+    rdtsc  ;TSC를 읽어서 RDX:RAX 에 저장                   
+    
+    shl rdx, 32               ;RDX 레지스터에 있는 상위 32비트 TSC 값과 RAX 레지스터에 있는 하위 32비트 TSC값을 
+    or rax, rdx               ;OR하여 RAX 레지스터에 64비트 TSC 값을 저장
+                              
+    pop rdx
+    ret
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; 태스크 관련 어셈블리어 함수
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Context를 저장하고 셀렉터를 교체하는 매크로
+%macro KSAVECONTEXT 0         
+     ;RBP 레지스터부터 GS 세그먼트 셀렉터까지 모두 스택에 삽입 
+    push rbp
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    
+    mov ax, ds        
+    push rax          
+    mov ax, es
+    push rax
+    push fs
+    push gs 
+%endmacro         
+
+;Context를 복원하는 매크로
+%macro KLOADCONTEXT 0     
+     ;GS 세그먼트 셀렉터부터 RBP 레지스터까지 모두 스택에서 꺼내 복원
+    pop gs
+    pop fs
+    pop rax
+    mov es, ax        
+    pop rax           
+    mov ds, ax
+    
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rsi
+    pop rdi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    pop rbp        
+%endmacro         
+
+;Current Context에 현재 Context를 저장하고 Next Task에서 Context를 복구
+;   PARAM: Current Context, Next Context
+kSwitchContext:
+    push rbp          
+    mov rbp, rsp
+    
+    pushfq            
+    cmp rdi, 0        
+    je .LoadContext
+    popfq             
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; 현재 태스크의 콘텍스트를 저장
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    push rax    ;Context 영역의 오프셋으로 사용할 rax 레지스터를 스택에 저장          
+    
+    ;SS,RSP,RFLAGS,CS,RIP 레지스터 순서대로 삽입
+    mov ax, ss                            
+    mov qword[ rdi + ( 23 * 8 ) ], rax
+
+    mov rax, rbp                        ;RBP에 저장된 RSP 레지스터 저장              
+    add rax, 16                         ;push rbp 와 Return Address를 제외한 값으로 저장
+    mov qword[ rdi + ( 22 * 8 ) ], rax    
+    
+    pushfq                                
+    pop rax
+    mov qword[ rdi + ( 21 * 8 ) ], rax
+
+    mov ax, cs                            
+    mov qword[ rdi + ( 20 * 8 ) ], rax
+
+    mov rax, qword[ rbp + 8 ]           ;RIP 레지스터를 Return Address로 설정하여
+    mov qword[ rdi + ( 19 * 8 ) ], rax  ;다음 콘텍스트 복원 시에 이 함수를 호출한 위치로 이동하게 함
+
+
+    pop rax
+    pop rbp
+    
+    ;스택을 변경
+    add rdi, ( 19 * 8 )
+    mov rsp, rdi
+    sub rdi, ( 19 * 8 )
+      
+    KSAVECONTEXT ;저장
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; 다음 태스크의 콘텍스트 복원 
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.LoadContext:
+    mov rsp, rsi
+    
+    KLOADCONTEXT ;복원
+    iretq
+
+;프로세서를 쉬게 함
+kHlt:
+    hlt       
+    hlt
+    ret
+    
+;PARAM: 값을 저장할 어드레스(Destination, rdi), 비교할 값(Compatr, rsi), 설정할 값(Source, rdx) 
+kTestAndSet:
+    mov rax, rsi          ;두 번째 파라미터인 비교할 값을 RAX 레지스터에 저장
+    
+    ;RAX 레지스터에 저장된 비교할 값과 첫 번째 파라미터의 메모리 어드레스의 값을 비교하여
+    ;두 값이 같다면 세 번째 파라미터의 값을 첫 번째 파라미터가 가리키는 어드레스에 삽입 
+    lock cmpxchg byte [ rdi ], dl
+    je .SUCCESS           ;ZF 비트가 1이면 같다는 뜻이므로 SUCCESS
+    
+.NOTSAME:                 
+    mov rax, 0x00
+    ret
+    
+.SUCCESS:                 
+    mov rax, 0x01
     ret
